1134c942a2d3b78a02f384a840dff4c44f7da540
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-06-30 13:21:02 -0300

    feat: class contaCorrente

--- ex-heranca.py
+++ ex-heranca.py
@@ -1,7 +1,7 @@
 import abc
 
 class Conta(abc.ABC):
-    def __init__(self, agencia, conta, saldo):
+    def __init__(self, agencia, conta, saldo = 0):
         self.agencia = agencia
         self.conta = conta
         self.saldo = saldo
@@ -29,11 +29,35 @@ class ContaPoupanca(Conta):
         print("Não foi possível sacar o valor desejado")
         self.detalhes(f"(Saque Negado {valor})")
 
+class ContaCorrente(Conta):
+    def __init__(self, agencia, conta, saldo = 0, limite = 0):
+        super().__init__(agencia, conta, saldo)
+        self.limite = limite
+
+    def sacar(self, valor):
+        valor_pos_saque = self.saldo - valor
+        limite_maximo = -self.limite
+
+        if(valor_pos_saque >= limite_maximo):
+            self.saldo -= valor
+            self.detalhes(f"(Saque {valor})")
+            return self.saldo
+        
+        print("Não foi possível sacar o valor desejado")
+        print(f"Seu limite é {-self.limite:.2f}")
+        self.detalhes(f"(Saque Negado {valor})")
+
+
 if __name__ == "__main__":
     cp1 = ContaPoupanca(111, 222, 0)
     cp1.sacar(1)
     cp1.depositar(1)
     cp1.sacar(1)
+    print("##")
+    cc1 = ContaPoupanca(111, 222, 0)
+    cc1.sacar(1)
+    cc1.depositar(1)
+    cc1.sacar(1)
 
 
     
